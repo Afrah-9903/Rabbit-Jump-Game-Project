4fb4d82ff558b9e7323aa09cbde0b313812156d9
Afrah Mohammed <af.moali99@gmail.com>
2024-05-08 16:00:57 +0300

    move character

--- game.js
+++ game.js
@@ -1,12 +1,10 @@
 let image = document.getElementById("image")
-let block = document.getElementById("block")
-
 let jump = document.getElementById("jump")
 
 document.addEventListener("DOMContentLoaded", function () {
   let character = document.getElementById("character")
-  let carrots = document.querySelectorAll(".carrot")
-  let score = 0
+  let carrotContainers = document.querySelectorAll(".carrotContainer")
+
   function triggerjump() {
     character.classList.add("jump")
     setTimeout(() => {
@@ -16,18 +14,30 @@ document.addEventListener("DOMContentLoaded", function () {
   function handlekeypress(event) {
     if (event.code === "Space") {
       triggerjump()
-      generateCarrot()
+      shootCarrot()
+      generateCarrot(carrotContainers)
     }
   }
-  function generateCarrot() {
-    const carrotContainer = document.getElementById("carrrotContainer") // gets the element in  html id with the carrot container
-    const carrot = document.createElement("div") // used to create new div in element memory.
-    carrot.classList.add("carrot") //adding css to the newly created div which is  used to style the carrot.
-    const x = Math.random() * (carrotContainer.clientWidth - 30) // generate a random x co-ordinate for the carrot within the width of the carrotContainer and subtract 30 from the width to ensure that the carrot stays within the container bounds.
-    const y = Math.random() * (carrotContainer.clientHeight - 30) // generate a random y co-ordinate for the carrot within the height of the cvarrotContainer and subtract 30 to  ensure the carrot stayes within the container bounds.
-    carrot.style.left = x + "px" // set the left css prpty to the carrot div to the randomly generate x  co-ordinate. This determines the horizontal position of the carrot within its  container.
-    carrot.style.top = x + "px" // set the top css prpty to the carrot to the randomly generate y co-ordinate. This determines the vertical position of the carrot within its  container.
-    carrotContainer.appendChild(carrot) // finally we append the newly created carrot div to the carrotContainer element in the html. This adds the carrot visually on the screen.
+  function generateCarrot(carrotContainers) {
+    //gets a random index to select a carrot container.
+    const ramdomIndex = Math.floor(Math.random() * carrotContainers.length)
+    // Selects the carrot container at the random index.
+    const carrotContainer = carrotContainers[ramdomIndex] // used to create  a new element div for the carrot.
+    const carrot = document.createElement("div") //adding css to the newly created div which is  used to style the carrot.
+    carrot.classList.add("carrot")
+    // append the carrot to the selected carrot container.
+    carrotContainer.appendChild(carrot)
+    // Append the carrot to the selected carrot container.
+  }
+  function shootCarrot() {
+    let carrot = document.createElement("div")
+    carrot.classList.add("carrot", "shoot")
+    carrotContainers[0].apendChild(carrot)
+
+    setTimeout(() => {
+      // remove the carrot after it's shot
+      carrot.remove()
+    }, 1000)
   }
 
   document.addEventListener("keyup", handlekeypress)
